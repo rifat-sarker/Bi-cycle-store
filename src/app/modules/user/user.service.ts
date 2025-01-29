f13a1d1b5d6fcef9cacb536bd6c67e586dcf0ac0
Rifat Sarker <rifatswd@gmail.com>
2025-01-29 13:27:25 +0600

    enhance user service: add user existence check and token generation upon creation

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,11 +1,41 @@
-
+import httpStatus from 'http-status';
+import config from '../../config';
+import AppError from '../../errors/AppError';
 import { TAdmin } from '../admin/admin.interface';
+import { createToken } from '../auth/auth.utils';
 import { TUser } from './user.interface';
 import { User } from './user.model';
 
 const createUserIntoDB = async (userData: TUser) => {
-  const result = await User.create(userData);
-  return result;
+  // 🔹 Check if user already exists
+  const existingUser = await User.isUserExistsByEmail(userData.email);
+
+  if (existingUser) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      'This email is already registered!',
+    );
+  }
+
+  // 🔹 Create new user in the database
+  const newUser = await User.create(userData);
+
+  // 🔹 Generate token for the newly created user
+  const jwtPayload = {
+    email: newUser.email,
+    role: newUser.role,
+  };
+
+  const accessToken = createToken(
+    jwtPayload,
+    config.jwt_access_secret as string,
+    config.jwt_access_expires_in as string,
+  );
+
+  return {
+    ...newUser.toObject(),
+    accessToken
+  };
 };
 
 const createAdminIntoDB = async (userData: TAdmin) => {
